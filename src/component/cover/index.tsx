0c3b35a1c741e3aa26af42295f60964dfef18508
takjoolove25 <takjoolove25@gmail.com>
2026-03-23 10:13:49 +0900

    first commit

---
+++ src/component/cover/index.tsx
@@ -0,0 +1,47 @@
+import {
+  BRIDE_FULLNAME,
+  GROOM_FULLNAME,
+  LOCATION,
+  WEDDING_DATE,
+  WEDDING_DATE_FORMAT,
+} from "../../const"
+import { COVER_IMAGE } from "../../images"
+import { LazyDiv } from "../lazyDiv"
+
+const DAY_OF_WEEK = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+]
+
+export const Cover = () => {
+  return (
+    <LazyDiv className="card cover">
+      <div className="wedding-date">
+        {WEDDING_DATE.format("YYYY")}
+        <div className="divider" />
+        {WEDDING_DATE.format("MM")}
+        <div className="divider" />
+        {WEDDING_DATE.format("DD")}
+      </div>
+      <div className="wedding-day-of-week">
+        {DAY_OF_WEEK[WEDDING_DATE.day()]}
+      </div>
+      <div className="image-wrapper">
+        <img src={COVER_IMAGE} alt="sample" />
+      </div>
+      <div className="subtitle">Save the date for the wedding of</div>
+      <div className="names">
+        {GROOM_FULLNAME}
+        <div className="divider" />
+        {BRIDE_FULLNAME}
+      </div>
+      <div className="info">{WEDDING_DATE.format(WEDDING_DATE_FORMAT)}</div>
+      <div className="info">{LOCATION}</div>
+    </LazyDiv>
+  )
+}
