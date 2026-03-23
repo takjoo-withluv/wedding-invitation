92380c8d121dd553c04bc25003f12a183fed18f2
takjoolove25 <takjoolove25@gmail.com>
2026-03-23 13:28:12 +0900

    update

--- src/component/information/index.tsx
+++ src/component/information/index.tsx
@@ -12,7 +12,7 @@ export const Information1 = () => {
       <div className="info-card">
         <div className="label">식사 안내</div>
         <div className="content">
-          식사시간: 13시 30분 ~ 15시 30분
+          식사시간: 13시 00분 ~ 15시 00분
           <br />
           장소: 3층 연회장
         </div>
