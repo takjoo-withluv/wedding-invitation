c2060706f669fddde1d53092e9e3ada3beae5bfc
takjoo-withluv <takjoolove0705@gmail.com>
2026-03-31 13:32:23 +0900

    update

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -21,8 +21,8 @@ export const Invitation = () => {
   const { openModal, closeModal } = useModal()
   return (
     <LazyDiv className="card invitation">
-      {/*<h2 className="english">Invitation</h2>*/}
-      <h2 className="english">소중한 당신을 초대합니다</h2>
+      {/*<h2 className="english">소중한 당신을 초대합니다</h2>*/}
+      <h2 className="english">Invitation</h2>
 
       <div className="break" />
 
