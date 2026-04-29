7b7a405fc9f65bd086b7429e947447363c4a5535
takjoo-withluv <takjoolove0705@gmail.com>
2026-04-29 14:57:52 +0900

    update

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -21,7 +21,6 @@ export const Invitation = () => {
   const { openModal, closeModal } = useModal()
   return (
     <LazyDiv className="card invitation">
-      {/*<h2 className="english">소중한 당신을 초대합니다</h2>*/}
       <h2 className="english">Invitation</h2>
 
       <div className="break" />
@@ -56,7 +55,87 @@ export const Invitation = () => {
 
       <div className="break" />
 
-      
+      {/* 연락하기 버튼 주석 처리 시작
+      <Button
+        onClick={() => {
+          openModal({
+            className: "contact-modal",
+            closeOnClickBackground: true,
+            header: (
+              <div className="title-group">
+                <div className="title">축하 인사 전하기</div>
+                <div className="subtitle">
+                  전화, 문자메세지로 축하 인사를 전해보세요.
+                </div>
+              </div>
+            ),
+            content: (
+              <>
+                <div className="contact-info">
+                  {GROOM_INFO.filter(({ phone }) => !!phone).map(
+                    ({ relation, name, phone }) => (
+                      <Fragment key={relation}>
+                        <div className="relation">{relation}</div>
+                        <div>{name}</div>
+                        <div>
+                          <PhoneIcon
+                            className="flip icon"
+                            onClick={() => {
+                              window.open(`tel:${phone}`, "_self")
+                            }}
+                          />
+                          <EnvelopeIcon
+                            className="icon"
+                            onClick={() => {
+                              window.open(`sms:${phone}`, "_self")
+                            }}
+                          />
+                        </div>
+                      </Fragment>
+                    ),
+                  )}
+                </div>
+                <div className="contact-info">
+                  {BRIDE_INFO.filter(({ phone }) => !!phone).map(
+                    ({ relation, name, phone }) => (
+                      <Fragment key={relation}>
+                        <div className="relation">{relation}</div>
+                        <div>{name}</div>
+                        <div>
+                          <PhoneIcon
+                            className="flip icon"
+                            onClick={() => {
+                              window.open(`tel:${phone}`, "_self")
+                            }}
+                          />
+                          <EnvelopeIcon
+                            className="icon"
+                            onClick={() => {
+                              window.open(`sms:${phone}`, "_self")
+                            }}
+                          />
+                        </div>
+                      </Fragment>
+                    ),
+                  )}
+                </div>
+              </>
+            ),
+            footer: (
+              <Button
+                buttonStyle="style2"
+                className="bg-light-grey-color text-dark-color"
+                onClick={closeModal}
+              >
+                닫기
+              </Button>
+            ),
+          })
+        }}
+      >
+        연락하기
+      </Button>
+      연락하기 버튼 주석 처리 끝 */}
     </LazyDiv>
   )
 }
