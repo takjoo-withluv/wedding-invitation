3e3e206b6961a79b120baffe6351eb5f71e1992c
takjoo-withluv <takjoolove0705@gmail.com>
2026-04-29 14:38:09 +0900

    update

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -16,61 +16,62 @@ export const Location = () => {
         <Map />
       </LazyDiv>
 
-      <LazyDiv className="card location-detail">
+      <LazyDiv className="card location">
         {/* 대중교통 섹션 */}
         <div className="location-info">
-          <div className="title-group">
+          <div className="transportation-icon-wrapper">
             <BusIcon className="transportation-icon" />
-            <span className="heading">대중교통</span>
           </div>
-          
-          <div className="info-section">
-            <div className="sub-title">지하철</div>
-            <div className="content">
-              <p>
-                <span className="line line-1">1호선</span> 
-                <strong>범일역</strong> 2번, 8번 출구 (도보 7분)
-              </p>
-              <p>
-                <span className="line line-2">2호선</span> 
-                <strong>범일역</strong> 3번 출구 (시민회관 방면 도보 5분)
-              </p>
-            </div>
+          <div className="heading">대중교통</div>
+          <div />
+          <div className="content">
+            <b>[지하철 이용 시]</b>
+            <br />
+            1호선 <b>범일역 2번, 8번 출구</b> (도보 7분)
+            <br />
+            2호선 <b>범일역 3번 출구</b> (시민회관 방면)
           </div>
-
-          <div className="info-section">
-            <div className="sub-title">버스</div>
-            <div className="content bus-list">
-              <dl>
-                <dt>시민회관 / 자성대 / 국민은행 하차</dt>
-                <dd>일반: 10, 22, 23, 26, 27, 40, 41, 42, 57, 68, 80, 83, 83-1, 99, 101, 108, 134, 138, 168</dd>
-              </dl>
-            </div>
+          <div />
+          <div className="content">
+            <b>[버스 이용 시]</b>
+            <br />
+            하차 정류장: <b>시민회관, 자성대, 범일동 국민은행</b>
+            <br />
+            <small style={{ fontSize: '0.85em', color: '#888' }}>
+              22, 23, 26, 27, 40, 41, 42, 57, 68, 80, 83, 83-1, 99, 101, 108, 134, 138, 168
+            </small>
           </div>
         </div>
 
-        <hr className="divider" />
-
         {/* 자가용 섹션 */}
         <div className="location-info">
-          <div className="title-group">
+          <div className="transportation-icon-wrapper">
             <CarIcon className="transportation-icon" />
-            <span className="heading">자가용</span>
           </div>
-          
-          <div className="info-section">
-            <div className="content">
-              <p className="highlight">네비게이션에서 <strong>'디엘웨딩홀'</strong> 검색</p>
-              <ul className="guide-list">
-                <li>주차 요금은 <strong>무료</strong>입니다.</li>
-                <li>주차장 이용 시 웨딩홀과 바로 연결됩니다.</li>
-              </ul>
+          <div className="heading">자가용</div>
+          <div />
+          <div className="content">
+            <b>'디엘웨딩홀'</b> 검색
+            <br />
+            주차장 이용 시 웨딩홀과 바로 연결됩니다.
+            <br />
+            <span style={{ color: '#d9534f' }}><b>주차 요금은 무료입니다.</b></span>
+          </div>
+          <div />
+          <div className="content">
+            <div style={{ 
+              marginTop: '10px', 
+              padding: '10px', 
+              backgroundColor: '#f8f8f8', 
+              borderRadius: '5px',
+              fontSize: '0.9em' 
+            }}>
+              <b>※ 유의사항</b>
+              <br />
+              범일역 인근은 교통이 혼잡할 수 있으니 
+              <br />
+              가급적 대중교통 이용을 권장드립니다.
             </div>
-          </div>
-
-          <div className="notice-section">
-            <p className="notice-title">※ 유의사항</p>
-            <p className="notice-content">주말에는 웨딩홀 주변이 혼잡할 수 있으니 가급적 대중교통 이용을 권장드립니다.</p>
           </div>
         </div>
       </LazyDiv>
