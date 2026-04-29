e77d77393c28434ecc16aea19da24e43d27a5fb4
takjoo-withluv <takjoolove0705@gmail.com>
2026-04-29 15:46:09 +0900

    update

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -27,51 +27,55 @@ export const Location = () => {
           <div className="content">
             <b>[지하철 이용 시]</b>
             <br />
-            1호선 <b>범일역 2번, 8번 출구</b>
+            1호선 <b>범일역 하차</b> - 2번 출구 (도보 7분)
             <br />
-            2호선 <b>문현역 3번 출구</b>
+            2호선 <b>문현역 하차</b> - 3번 출구 (도보 5분)
           </div>
           <div />
           <div className="content">
             <b>[버스 이용 시]</b>
             <br />
-            <span style={{ color: '#888', fontSize: '0.9em' }}>• 대신, 남포, 부산역 방면 (자성대/시민회관 하차)</span>
+            <span style={{ color: '#888', fontSize: '0.9em' }}>• 대신, 남포, 중앙동, 부산역 방면</span>
             <br />
-            &nbsp;&nbsp;22, 26, 27, 40, 41, 101, 134
+            &nbsp;&nbsp;26, 41 (부산진성공원) / 22, 27, 40, 101, 134 (시민회관)
             <br />
-            <span style={{ color: '#888', fontSize: '0.9em' }}>• 해운대, 광안, 대연 방면 (자성대/시민회관 하차)</span>
+            <span style={{ color: '#888', fontSize: '0.9em' }}>• 해운대, 센텀, 수영 방면</span>
             <br />
-            &nbsp;&nbsp;40, 41, 42, 83, 83-1, 108
+            &nbsp;&nbsp;40, 42 (부산진성공원)
             <br />
-            <span style={{ color: '#888', fontSize: '0.9em' }}>• 사상, 서면, 당감 방면 (국민은행/시민회관 하차)</span>
+            <span style={{ color: '#888', fontSize: '0.9em' }}>• 민락, 광안, 대연, 용호동 방면</span>
             <br />
-            &nbsp;&nbsp;23, 68, 108, 138, 168
+            &nbsp;&nbsp;40, 41, 42, 83, 108, 22, 27 (부산진성공원) / 83-1 (시민회관)
             <br />
-            <span style={{ color: '#888', fontSize: '0.9em' }}>• 노포, 동래, 양정 방면 (시민회관/국민은행 하차)</span>
+            <span style={{ color: '#888', fontSize: '0.9em' }}>• 사상, 엄궁, 서면, 당감동 방면</span>
             <br />
-            &nbsp;&nbsp;10, 57, 80, 99
+            &nbsp;&nbsp;23, 108, 138 (국민은행) / 68, 168 (시민회관)
+            <br />
+            <span style={{ color: '#888', fontSize: '0.9em' }}>• 노포, 동래, 연산, 양정 방면</span>
+            <br />
+            &nbsp;&nbsp;10 (국민은행) / 57, 80, 99 (시민회관)
           </div>
         </div>
 
-        {/* 자가용 및 주차 섹션 */}
+        {/* 주차 및 문의 섹션 */}
         <div className="location-info">
           <div className="transportation-icon-wrapper">
             <CarIcon className="transportation-icon" />
           </div>
-          <div className="heading">자가용 및 주차</div>
+          <div className="heading">주차 안내</div>
           <div />
           <div className="content">
-            <b>내비게이션: '디엘웨딩홀' 검색</b>
+            <b>동일타워 내 지하주차장 이용 가능</b>
             <br />
-            (부산 동구 조방로 14 동일타워 3층)
+            - 예식 하객 기준 <b>2시간 무료 주차</b> 제공
+            <br />
+            - 넉넉한 주차 공간으로 편하게 방문하실 수 있습니다.
           </div>
           <div />
           <div className="content">
-            <b>동일타워 내 대형 주차장 완비</b>
+            <b>[문의 사항]</b>
             <br />
-            - 하객 분들을 위한 <b>넉넉한 주차 공간</b>이 마련되어 있습니다.
-            <br />
-            - <b>무료 주차 제공</b> 및 웨딩홀 직통 엘리베이터 연결
+            디엘웨딩홀: 051-638-6100, 6200
           </div>
         </div>
       </LazyDiv>
