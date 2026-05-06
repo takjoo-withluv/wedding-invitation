527a5d701fdb4ddf0785faac549762ae48339d68
takjoo-withluv <takjoolove0705@gmail.com>
2026-05-06 17:26:34 +0900

    update

--- src/images/index.ts
+++ src/images/index.ts
@@ -10,7 +10,6 @@ import image8 from "./image8.png"
 import image9 from "./image9.png"
 import image10 from "./image10.png"
 import image11 from "./image11.png"
-import image12 from "./image12.png"
 
 
 export const COVER_IMAGE = coverImage
@@ -26,6 +25,5 @@ export const GALLERY_IMAGES = [
   image8,
   image9,
   image10,
-  image11,
-  image12
+  image11
 ]
