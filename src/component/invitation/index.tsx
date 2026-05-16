d4ff7b8339b62527e940125e4659c6a7cdac7e2b
takjoo-withluv <takjoolove0705@gmail.com>
2026-05-16 19:06:06 +0900

    update

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -25,16 +25,16 @@ export const Invitation = () => {
 
       <div className="break" />
 
-      <div className="content">싱그러운 여름 향기 가득한 날</div>
-      <div className="content">소중한 분들을 모시고</div>
+      <div className="content">오랜 시간 친구처럼</div>
+      <div className="content">때로는 연인처럼 함께 걸어온 저희가</div>
       <div className="content">사랑의 약속을 하려고 합니다.</div>
       <div className="break" />
-      <div className="content">햇살이 뜨거울 땐 가려주고,</div>
-      <div className="content">비가 오면 우산이 되어주는</div>
+      <div className="content">이제 부부라는 이름으로</div>
+      <div className="content">같은 하루를 살아가려 합니다.</div>
       <div className="content">부부가 되겠습니다.</div>
       <div className="break" />
       <div className="content">기쁜날 함께 하시어</div>
-      <div className="content">저희의 앞날을 축복해 주세요.</div>
+      <div className="content">저희의 시작을 축복해 주세요.</div>
 
       <div className="break" />
 
